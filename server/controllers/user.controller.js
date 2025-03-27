82fbaada1ed2d16db91245e063d3ddbcf2a95e06
Paul Philips <paulphilips@email.com>
2025-03-27 18:35:15 +0530

    Step 18 completed: - Added register user functionality - Updated user.model.js and added password hashing functionality using monoose hooks. - Created generate-token.util.js and added token cookie creation and sending logic - Added utils folder and referenced it in jsconfig.json and package.json

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import UserModel from "#models/user.model.js";
+import generateToken from "#utils/generate-token.util.js";
 
 /**
  * @desc Auth User
@@ -10,15 +11,7 @@ const authUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await UserModel.findOne({ email });
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
+    generateToken(res, user._id);
     res.json({
       _id: user._id,
       name: user.name,
@@ -37,7 +30,29 @@ const authUser = async (req, res) => {
  * @access public
  */
 const registerUser = async (req, res) => {
-  res.send("register");
+  const { name, email, password } = req.body;
+
+  const userExist = await UserModel.findOne({ email });
+
+  if (userExist) {
+    res.status(400); // Bad Request (client Error)
+    throw new Error("User already exists");
+  }
+
+  const user = await UserModel.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Data");
+  }
 };
 
 /**
@@ -108,4 +123,4 @@ const deleteUser = async (req, res) => {
   res.send("Delete user");
 };
 
-export { authUser, logoutUser };
+export { authUser, logoutUser, registerUser };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,17 @@
 import express from "express";
 
 // Users controlloers
-import { authUser, logoutUser } from "#controllers/user.controller.js";
+import {
+  authUser,
+  logoutUser,
+  registerUser,
+} from "#controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/login", authUser);
 router.post("/logout", logoutUser);
+// Registrations
+router.route("/").post(registerUser);
 
 export default router;
