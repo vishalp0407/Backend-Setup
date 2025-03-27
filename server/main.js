46616ad4b0ec15d3c7ab1ac4802fe8ec79eb8e18
Paul Philips <paulphilips@email.com>
2025-03-27 17:10:51 +0530

    Step 14 completed: - Created the login user controller implementation - Created the matchPassword mongoose helper method

--- server/main.js
+++ server/main.js
@@ -4,21 +4,24 @@ import morgan from "morgan";
 import colors from "colors";
 
 // import products from "#data/products.js";
-import productRoutes from "#routes/product.route.js";
 import { errorHandler } from "#middlewares/error.middleware.js";
 import connectDB from "#config/db.config.js";
+import productRoutes from "#routes/product.route.js";
+import userRoutes from "#routes/user.route.js";
 
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json()); //HTTP REQUEST BODY PARSING
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/users", userRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,1 +1,10 @@
-// User ROutes
+import express from "express";
+
+// Users controlloers
+import { authUser } from "#controllers/user.controller.js";
+
+const router = express.Router();
+
+router.post("/login", authUser);
+
+export default router;
